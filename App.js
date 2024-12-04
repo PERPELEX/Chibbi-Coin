112fc7fe94c64970a2451199b9462bcaf0263184
PERPELEX <hannanamir18@gmail.com>
2024-12-04 19:41:35 +0500

    Added login stack

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { Provider as PaperProvider } from "react-native-paper";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import { ThemeProvider, ThemeContext } from "./components/theme/themeContext";
-import HomeScreen from "./components/screens/HomeScreen";
-import DetailsScreen from "./components/screens/DetailsScreen";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Provider as PaperProvider } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { ThemeProvider, ThemeContext } from './components/theme/themeContext';
+import HomeScreen from './components/screens/HomeScreen';
+import DetailsScreen from './components/screens/DetailsScreen';
+import LoginScreen from './components/screens/LoginScreen';
 
 const Tab = createMaterialBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#f0edf6"
+      inactiveColor="#3e2465"
+      barStyle={{ backgroundColor: '#694fad' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <Icon name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{
+          tabBarLabel: 'Details',
+          tabBarIcon: ({ color }) => (
+            <Icon name="details" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -16,33 +51,18 @@ export default function App() {
         {({ theme }) => (
           <PaperProvider theme={theme}>
             <NavigationContainer>
-              <Tab.Navigator
-                initialRouteName="Home"
-                activeColor={theme.colors.primary}
-                inactiveColor={theme.colors.placeholder}
-                barStyle={{ backgroundColor: theme.colors.surface }}
-              >
-                <Tab.Screen
-                  name="Home"
-                  component={HomeScreen}
-                  options={{
-                    tabBarLabel: "Home",
-                    tabBarIcon: ({ color }) => (
-                      <Icon name="home" color={color} size={26} />
-                    ),
-                  }}
+              <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen
+                  name="Login"
+                  component={LoginScreen}
+                  options={{ headerShown: false }}
                 />
-                <Tab.Screen
-                  name="Details"
-                  component={DetailsScreen}
-                  options={{
-                    tabBarLabel: "Details",
-                    tabBarIcon: ({ color }) => (
-                      <Icon name="details" color={color} size={26} />
-                    ),
-                  }}
+                <Stack.Screen
+                  name="Main"
+                  component={MainTabs}
+                  options={{ headerShown: false }}
                 />
-              </Tab.Navigator>
+              </Stack.Navigator>
             </NavigationContainer>
           </PaperProvider>
         )}
